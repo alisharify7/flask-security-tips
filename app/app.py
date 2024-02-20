fe1e4c25b992691c66880a13baae19aabd09bb07
ali sharifi <alisharifyofficial@gmail.com>
2024-02-20 20:02:30 +0330

    update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,28 @@
+import os
+import datetime
 from flask import Flask, render_template
 from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 
-app = Flask(__name__) 
-app.config['SECRET_KEY'] = 'Secret key'
+class SessionConf:
+    # session cookie setting
+    SESSION_TYPE = "filesystem" # or redis or other db drivers
+    SESSION_PERMANENT = False
+    PERMANENT_SESSION_LIFETIME = datetime.timedelta(minutes=16)
+    SESSION_COOKIE_SECURE = True
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_NAME = 'session-name'
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Secret key' or os.urandom(24)
+app.secret_key = "Secret_KEY"
 csrf = CSRFProtect(app=app)
-
+Session(app=app)
 
 @app.route("/",  methods=["GET"]) 
 def index_view_page():
